8e27f954035bb6b5bdd33588bbc1e50696762eae
Samuel Schüpbach <schuepbs@gmail.com>
2021-05-18 19:26:16 +0000

    Resolve "Replace boolean return of guard with something more descriptive"

--- sfsm-base/src/lib.rs
+++ sfsm-base/src/lib.rs
@@ -19,6 +19,47 @@ pub trait State {
     fn exit(&mut self) {}
 }
 
+/// Enum used to indicate to the guard function if the transition should transit to the
+/// next state or remain in the current one.
+/// ```ignore
+/// impl Transition<Bar> for Foo {
+///     fn guard(&self) -> TransitGuard {
+///         if self.foo == 0 {
+///             TransitGuard::Transit
+///         } else {
+///             TransitGuard::Transit
+///         }
+///     }
+/// }
+/// ```
+#[derive(PartialEq)]
+pub enum TransitGuard {
+    /// Remains in the current state
+    Remain,
+    // Transits into the next state
+    Transit
+}
+
+/// Implements from<bool> trait for use of use.
+/// This allows to transit by returning true. Which simplify the code since it allows to return the
+/// TransitGuard from a simple comparison.
+/// ```ignore
+/// impl Transition<Bar> for Foo {
+///     fn guard(&self) -> TransitGuard {
+///         self.foo == 0 // Transits when self.foo == 0
+///     }
+/// }
+/// ```
+impl From<bool> for TransitGuard {
+    fn from(transit: bool) -> Self {
+        if transit {
+            TransitGuard::Transit
+        } else {
+            TransitGuard::Remain
+        }
+    }
+}
+
 /// Trait that must be implemented by a state that want to transition to DestinationState.
 ///
 /// All states can have none or many transitions.
@@ -43,13 +84,13 @@ pub trait Transition<DestinationState>: Into<DestinationState> {
     /// Specifies when the state has to transit. As long as the guard returns false, the state
     /// stays in the current state. When true is returned, the state machine will transit to
     /// DestinationState
-    fn guard(&self) -> bool;
+    fn guard(&self) -> TransitGuard;
 }
 
 // Test the concept
 #[cfg(test)]
 mod tests {
-    use crate::{State, Transition};
+    use crate::{State, Transition, TransitGuard};
     use std::rc::Rc;
     use std::cell::RefCell;
 
@@ -76,8 +117,8 @@ mod tests {
 
     impl Transition<ProcessData> for InitData {
         // Transit immediately
-        fn guard(&self) -> bool {
-            true
+        fn guard(&self) -> TransitGuard {
+            TransitGuard::Transit
         }
     }
 
@@ -113,14 +154,20 @@ mod tests {
 
     impl Transition<ProcessData> for ProcessData {
         // Transit immediately
-        fn guard(&self) -> bool {
-            return self.global.val == 1;
+        fn guard(&self) -> TransitGuard {
+            if self.global.val == 1 {
+                return TransitGuard::Transit;
+            }
+            return TransitGuard::Remain;
         }
     }
 
     impl Transition<InitData> for ProcessData {
-        fn guard(&self) -> bool {
-            return self.global.val == 2;
+        fn guard(&self) -> TransitGuard {
+            if self.global.val == 2 {
+                return TransitGuard::Transit;
+            }
+            return TransitGuard::Remain;
         }
     }
 
@@ -162,7 +209,7 @@ mod tests {
                     State::execute(&mut state);
                     Transition::<ProcessData>::execute(&mut state);
 
-                    if Transition::<ProcessData>::guard(&state) {
+                    if Transition::<ProcessData>::guard(&state) == TransitGuard::Transit {
 
                         State::exit(&mut state);
                         Transition::<ProcessData>::exit(&mut state);
@@ -190,7 +237,7 @@ mod tests {
                     Transition::<InitData>::execute(&mut state);
                     Transition::<ProcessData>::execute(&mut state);
 
-                    if Transition::<InitData>::guard(&state) {
+                    if Transition::<InitData>::guard(&state) == TransitGuard::Transit {
 
                         State::exit(&mut state);
                         Transition::<InitData>::exit(&mut state);
@@ -200,7 +247,7 @@ mod tests {
 
                         self.do_entry = true;
                         SfsmStates::InitStateEntry(Some(next_state_data))
-                    } else if Transition::<ProcessData>::guard(&state) {
+                    } else if Transition::<ProcessData>::guard(&state) == TransitGuard::Transit {
 
                         State::exit(&mut state);
                         Transition::<InitData>::exit(&mut state);
